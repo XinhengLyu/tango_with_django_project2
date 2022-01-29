e087daf15e2fd7bc071d1f08653bdd0bed7757d3
Xinheng Lyu <2642528L@student.gla.ac.uk>
2022-01-29 13:04:18 +0000

    2

--- rango/urls.py
+++ rango/urls.py
@@ -5,5 +5,7 @@ from rango import views
 app_name = 'rango'
 
 urlpatterns=[
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+    path('about/',views.about,name='about')
+    
 ]
--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
-
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
+    
+def about(request):
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/about.html', context=context_dict)
 # Create your views here.
