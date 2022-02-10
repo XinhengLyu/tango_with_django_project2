2449ba4d72af78354d3b261844fbd52dcc6ff468
Xinheng Lyu <2642528L@student.gla.ac.uk>
2022-02-10 15:27:14 +0000

    02.10

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from rango.models import Category, Page
+
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
+
+admin.site.register(Category) 
+admin.site.register(Page,PageAdmin)
--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
+from rango.models import Category
+
 
 def index(request):
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    # Query the database for a list of ALL categories currently stored.
+    # Order the categories by the number of likes in descending order.
+    # Retrieve the top 5 only -- or all if less than 5.
+    # Place the list in our context_dict dictionary (with our boldmessage!) # that will be passed to the template engine.
+
+    category_list = Category.objects.order_by('-likes')[:5]
+    
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
+    # Render the response and send it back!
     return render(request, 'rango/index.html', context=context_dict)
     
 def about(request):
